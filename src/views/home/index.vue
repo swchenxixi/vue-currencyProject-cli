8acffb18ec60c4a87a6c66c86dd2e813b402f81c
lzg <672424568@qq.com>
2020-09-02 17:03:23 +0800

    update: 左侧菜单和tab页联动

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -19,6 +19,7 @@
             :pageList="pageList"
             :activeKey="activeKey"
             @change="onChangeTab"
+            @delete="onDeleteTab"
           ></tab-header>
           <div slot="main">主体区域</div>
         </tab-layout>
@@ -33,6 +34,8 @@ import SideMenu from '@/components/side-menu';
 import GlobalHeader from '@/components/global-header';
 import TabLayout from '@/layouts/tab-layout';
 import TabHeader from '@/components/tab-header';
+import { v4 as uuidv4 } from 'uuid';
+const uuid = uuidv4();
 export default {
   components: {
     SideMenu,
@@ -49,16 +52,31 @@ export default {
     };
   },
   created() {
-    this.getPermissions();
+    this.init();
   },
   methods: {
+    async init() {
+      await this.getPermissions();
+      this.initTabHeader();
+    },
+    initTabHeader() {
+      const { menus } = this;
+      if (menus && menus.length > 0) {
+        this.activeKey = menus[0].path;
+        this.pageList.push(menus[0]);
+      }
+    },
+    async getPermissions() {
+      const result = await this.$store.dispatch('loginUser/getPermissions');
+      this.menus = result.data.menus;
+    },
     toggleCollapsedMenu(value) {
       this.collapsed = value;
     },
     selectMenu(obj) {
       const { menus, findMenuByKey, pageList } = this;
       const menu = findMenuByKey(menus, obj.key);
-      if (!pageList.includes(menu)) {
+      if (menu && !pageList.includes(menu)) {
         pageList.push(menu);
       }
       this.activeKey = obj.key;
@@ -67,17 +85,31 @@ export default {
       for (let item of menus) {
         if (item.path === key) {
           return item;
-        } else if (item.children && item.children.length > 0) {
-          return this.findMenuByKey(item.children, key);
+        }
+        if (item.children && item.children.length > 0) {
+          const target = this.findMenuByKey(item.children, key);
+          if (target) return target;
         }
       }
     },
     onChangeTab(activeKey) {
       this.activeKey = activeKey;
     },
-    async getPermissions() {
-      const result = await this.$store.dispatch('loginUser/getPermissions');
-      this.menus = result.data.menus;
+    onDeleteTab(targetKey) {
+      const { pageList, activeKey } = this;
+      const targetIndex = pageList.findIndex(item => {
+        return item.path === targetKey;
+      });
+      //如果关闭的是首页
+      if (targetIndex === 0) {
+        this.$message.warning({ content: '首页不能关闭', key: uuid });
+        return;
+      }
+      //如果关闭的是当前激活的页面
+      if (activeKey === targetKey) {
+        this.activeKey = pageList[targetIndex - 1].path;
+      }
+      pageList.splice(targetIndex, 1);
     }
   }
 };
